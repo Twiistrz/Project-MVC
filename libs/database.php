13fddda7eb679bef78818e2d35f495c2e01630e2
Emmanuel See Te <Twiistrz@users.noreply.github.com>
2017-03-18 11:05:18 +0800

    Update database.php

--- libs/database.php
+++ libs/database.php
@@ -1,5 +1,5 @@
 <?php
 
 class Database extends PDO {
-	public function __construct() { parent::__construct(DBTYPE.':host='.DBHOST.';dbname='.DBNAME,DBUSER,DBPASS); }
+	public function __construct() { parent::__construct(DBTYPE.':host='.DBHOST.';dbname='.DBNAME.';charset=utf8',DBUSER,DBPASS,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); }
 }
