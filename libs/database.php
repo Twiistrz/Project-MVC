a5e77c20fe5e7b7ff1551c5e7771db37a03991c0
Emmanuel See Te <Twiistrz@users.noreply.github.com>
2017-03-18 11:08:07 +0800

    Update database.php
    Fixed to Display and Save Japanese and other Characters

--- libs/database.php
+++ libs/database.php
@@ -1,5 +1,6 @@
 <?php
 
+// Fixed to Display and Save Japanese and other Characters
 class Database extends PDO {
 	public function __construct() { parent::__construct(DBTYPE.':host='.DBHOST.';dbname='.DBNAME.';charset=utf8',DBUSER,DBPASS,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); }
 }
